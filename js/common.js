6e9c962c756e9dffe0aa2ff3d86534d878eda7ae
fad661 <syusuke.ueshima.ask@gmail.com>
2018-12-15 02:44:13 +0900

    原稿入れた

---
+++ js/common.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    var bandLists = document.getElementsByClassName( "band-list" );
+    console.log(bandLists);
+    Object.keys(bandLists).forEach(key => {
+        bandLists[key].onclick = function() {
+            this.classList.toggle('open');
+        }
+    });
+}
